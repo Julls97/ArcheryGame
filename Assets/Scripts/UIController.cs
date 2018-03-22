7e81888950e0d0a3da9ee10acb801b84c96291ec
Julls97 <yulia.jolie@yandex.ru>
2018-03-22 21:46:07 +0500

    База

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+	public void PlayFastGameButton() {
+		SceneManager.LoadScene("FastGame");
+	}
+	public void SettingsButton() {
+		SceneManager.LoadScene("SettingsMenu");
+	}
+	public void ExitButton() {
+		SceneManager.LoadScene("Menu");
+	}
+
+	void Start () {
+		
+	}
+	void Update () {
+		
+	}
+}
